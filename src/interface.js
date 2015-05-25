af459c2a160504e8e1b5a28beb77cb77d726fd7d
Anthony Domenico <mryesofyes@gmail.com>
2015-05-25 17:00:38 -0400

    makePad separated into makePad and makePad view. Drawing chords.

--- src/interface.js
+++ src/interface.js
@@ -2,16 +2,24 @@
 {
 	"use strict";
 
+	var makeNote = notepad.makeNote;
+	var notesFromIntervals = notepad.notesFromIntervals;
+
 	var gridShades = [200, 180, 230, 200];
 
-	var makePad = function(id, width, height)
+	var makePadView = function(id)
 	{
+		var pad;
 		var canvas = document.getElementById(id);
 		var ctx = canvas.getContext("2d");
 		var canvasWidth = parseInt(canvas.getAttribute("width"));
 		var canvasHeight = parseInt(canvas.getAttribute("height"));
-		var noteWidth = canvasWidth / width;
-		var noteHeight = canvasHeight / height;
+		var gridColumns = 30;
+		var gridRows = 20;
+		var noteWidth = canvasWidth / gridColumns;
+		var noteHeight = canvasHeight / gridRows;
+		var lastVisibleRow = 30;
+		var firstVisibleColumn = 0;
 
 		var colorStyle = function(r, g, b)
 		{
@@ -21,54 +29,142 @@
 		var colorStyleFromShade = function(shade)
 		{
 			return colorStyle(shade, shade, shade);
-		}
+		};
 
-		return {
-
-			canvas: canvas,
-			ctx: ctx,
-			width: width, height: height,
-			noteWidth: noteWidth, noteHeight: noteHeight,
-
-			init: function()
+		var drawGrid = function()
+		{
+			for (var y=0; y<gridRows; y+=2)
 			{
-				this.draw();
-			},
-
-			draw: function()
-			{
-				for (var y=0; y<this.height; y+=2)
+				for (var x=0; x<gridColumns; x+=2)
 				{
-					for (var x=0; x<this.width; x+=2)
-					{
-						var ctxPositionX = x * this.noteWidth;
-						var ctxPositionY = y * this.noteHeight;
-						ctx.fillStyle = colorStyleFromShade(gridShades[0]);
-						ctx.fillRect(
-							ctxPositionX, ctxPositionY,
-							this.noteWidth, this.noteHeight);
-						ctx.fillStyle = colorStyleFromShade(gridShades[1]);
-						ctx.fillRect(
-							ctxPositionX + this.noteWidth, ctxPositionY,
-							this.noteWidth, this.noteHeight);
-						ctx.fillStyle = colorStyleFromShade(gridShades[2]);
-						ctx.fillRect(
-							ctxPositionX, ctxPositionY + this.noteHeight,
-							this.noteWidth, this.noteHeight);
-						ctx.fillStyle = colorStyleFromShade(gridShades[3]);
-						ctx.fillRect(
-							ctxPositionX + this.noteWidth, ctxPositionY + this.noteHeight,
-							this.noteWidth, this.noteHeight);
-					}
+					var ctxPositionX = x * noteWidth;
+					var ctxPositionY = y * noteHeight;
+					ctx.fillStyle = colorStyleFromShade(gridShades[0]);
+					ctx.fillRect(
+						ctxPositionX, ctxPositionY,
+						noteWidth, noteHeight);
+					ctx.fillStyle = colorStyleFromShade(gridShades[1]);
+					ctx.fillRect(
+						ctxPositionX + noteWidth, ctxPositionY,
+						noteWidth, noteHeight);
+					ctx.fillStyle = colorStyleFromShade(gridShades[2]);
+					ctx.fillRect(
+						ctxPositionX, ctxPositionY + noteHeight,
+						noteWidth, noteHeight);
+					ctx.fillStyle = colorStyleFromShade(gridShades[3]);
+					ctx.fillRect(
+						ctxPositionX + noteWidth, ctxPositionY + noteHeight,
+						noteWidth, noteHeight);
 				}
 			}
 		};
-	}
+
+		var drawNotes = function(notes)
+		{
+			ctx.fillStyle = colorStyle(0, 0, 0);
+			for (var i=0; i<notes.length; i++)
+			{
+				var fullValue = notes[i].pitch.fullValue();
+				var duration = notes[i].durationInBeats;
+				var startTime = notes[i].startTimeInBeats;
+				ctx.fillRect(
+					startTime * noteWidth,
+					(canvasHeight - noteHeight) - fullValue * noteHeight,
+					duration * noteWidth,
+					noteHeight);
+			}
+		};
+
+		var draw = function(notes)
+		{
+			ctx.fillStyle = colorStyle(255, 255, 255);
+			ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+			drawGrid();
+			if (notes !== undefined)
+				drawNotes(notes);
+		};
+
+		var hasPad = function()
+		{
+			return typeof pad !== "undefined";
+		};
+
+		var onNoteAdded = function(note, allNotes)
+		{
+			draw(allNotes);
+		};
+
+		draw();
+
+		return {
+			draw: draw,
+			get pad()  { return pad; },
+			set pad(newPad)
+			{
+				pad = newPad;
+				pad.onNoteAdded.push(onNoteAdded);
+			},
+			hasPad: hasPad
+		};
+	};
+
+	var makePad = function()
+	{
+		var notes = [];
+		var tonicNote = makeNote(0, 4);
+
+		// parameters: (note, allNotes)
+		var onNoteAdded = [];
+
+		// parameters: (tonic)
+		var onTonicChanged = [];
+
+		var addNote = function(pitch, startTimeInBeats, durationInBeats)
+		{
+			var note = {
+				pitch: pitch,
+				startTimeInBeats: startTimeInBeats,
+				durationInBeats: durationInBeats};
+			notes.push(note);
+			for (var i=0; i<onNoteAdded.length; i++)
+				onNoteAdded[i](note, notes);
+		};
+
+		var addNotes = function(notes)
+		{
+			for (var i=0; i<notes.length; i++)
+				addNote(
+					notes[i].pitch,
+					notes[i].startTimeInBeats,
+					notes[i].durationInBeats);
+		};
+
+		return Object.freeze(
+		{
+			addNote: addNote, addNotes: addNotes,
+			onNoteAdded: onNoteAdded,
+			onTonicChanged: onTonicChanged,
+			get tonic() { return tonicNote; },
+			set tonic(newTonic)
+			{
+				tonicNote = newTonic;
+				for (var i=0; i<onTonicChanged.length; i++)
+					onTonicChanged[i](tonicNote);
+			}
+		});
+	};
 
 	window.onload = function()
 	{
-		var pad = makePad("interface", 40, 30);
-		pad.init();
+		var pad = makePad();
+		var padView = makePadView("interface");
+		padView.pad = pad;
+		var testChord = notesFromIntervals(notepad.chords.minor, makeNote(0, 0));
+		for (var i=0; i<testChord.length; i++)
+		{
+			console.log(testChord[i]);
+			pad.addNote(testChord[i], 0, 1);
+		}
 	};
 
 })();
--- src/music_functions.js
+++ src/music_functions.js
@@ -33,163 +33,7 @@ notepad.scales =
 	chromatic: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 };
 
-notepad.namedTuple = function(fields, initializer)
-{
-	if (!Array.isArray(fields))
-		throw new TypeError("Expected array.");
 
-	return function(params)
-	{
-		var object = {};
-
-		if (typeof arguments[0] === "object" && arguments.length < 2)
-		{
-			Object.keys(params).forEach(function(field)
-			{
-				if (fields.indexOf(field) < 0) return;
-				object[field] = params[field];
-			});
-		}
-		else
-		{
-			for (var i=0; i<arguments.length; i++)
-			{
-				if (fields[i] !== undefined)
-					object[fields[i]] = arguments[i];
-			}
-		}
-
-		if (initializer !== undefined)
-		{
-			var newObject = initializer(object);
-			if (newObject) object = newObject;
-		}
-
-		return Object.freeze(object);
-	};
-};
-
-notepad.valueFromLetter = function(letter)
-{
-	var notesInOctave = notepad.notesInOctave;
-
-	var validLetters = "CDEFGAB";
-	var symbol = (letter.length > 1) ? letter[1] : undefined;
-	letter = letter[0].toUpperCase();
-	if (symbol)
-		letter += symbol;
-	var letterIndex = validLetters.indexOf(letter[0]);
-	if (letterIndex < 0)
-		throw new TypeError("Must be a letter between A and G");
-	var halfSteps = [2, 2, 1, 2, 2, 2];
-	var value = 0;
-	for (var i=0; i<letterIndex; i++)
-		value += halfSteps[i];
-	if (symbol === "#")
-		value++;
-	if (symbol === "b")
-		value--;
-	return value % notesInOctave;
-};
-
-notepad.makeNote = notepad.namedTuple(["value", "octave"], function(obj)
-{
-	var valueFromLetter = notepad.valueFromLetter;
-	var notesInOctave = notepad.notesInOctave;
-
-	if (typeof obj.value !== "number")
-	{
-		if (typeof obj.value === "string")
-			obj.value = valueFromLetter(obj.value);
-		else throw new TypeError("\"value\" is a number or a letter");
-	}
-	if (typeof obj.octave !== "number")
-		throw new TypeError("\"octave\" is a number");
-	if (obj.value < 0 || obj.value > 11)
-		throw new RangeError("\"value\" is a number between 0 and 11");
-	if (obj.octave < 0)
-		throw new RangeError("\"octave\" is non-negative");
-
-	obj.toString = function()
-	{
-		var string = "";
-		for (var field in obj)
-		{
-			if (!obj.hasOwnProperty(field)) continue;
-			if (typeof obj[field] === "function") continue;
-			string += obj[field] + " ";
-		}
-		return string;
-	};
-
-	// If new, add to pool of note objects;
-	// otherwise, grab the existing one
-	var string = obj.toString();
-	if (notepad.notes[string] !== undefined)
-		return notepad.notes[string];
-	else notepad.notes[string] = obj;
-
-	obj.transpose = function(interval)
-	{
-		var makeNote = notepad.makeNote;
-
-		var fullValue = this.value + this.octave * notesInOctave;
-		fullValue += interval;
-		if (fullValue <= 0)
-			return makeNote(0, 0);
-		var newValue = fullValue % notesInOctave;
-		var newOctave = Math.floor(fullValue / notesInOctave);
-		return makeNote(newValue, newOctave);
-	};
-});
-
-notepad.notesFromIntervals = function(intervals, baseNote)
-{
-	var makeNote = notepad.makeNote;
-
-	var notes = [];
-	notes.push(makeNote(baseNote.value, baseNote.octave));
-	intervals.forEach(function(interval)
-	{
-		notes.push(baseNote.transpose(interval));
-	});
-	return notes;
-};
-
-notepad.chordIsSubset = function(chord, chordBaseNote, notes)
-{
-	var notesFromIntervals = notepad.notesFromIntervals;
-
-	var chordNotes = notesFromIntervals(chord, chordBaseNote);
-	// todo: find out why creating this within the loop is wrong
-	var existsTest = function(note)
-	{
-		return note.value === this.value;
-	};
-	for (var note in chordNotes)
-	{
-		if (!chordNotes.hasOwnProperty(note)) continue;
-		if (!notes.some(existsTest, chordNotes[note]))
-			return false;
-	}
-	return true;
-};
-
-notepad.compatibleChords = function(scale, scaleBaseNote, chordBaseNote)
-{
-	var chordIsSubset = notepad.chordIsSubset;
-	var chords = notepad.chords;
-
-	var scaleNotes = notepad.notesFromIntervals(scale, scaleBaseNote);
-	var compatibleList = [];
-	for (var chord in chords)
-	{
-		if (!chords.hasOwnProperty(chord)) continue;
-		if (chordIsSubset(chords[chord], chordBaseNote, scaleNotes))
-			compatibleList.push(chord);
-	}
-	return compatibleList;
-};
 
 // All note instances are here, so they can be compared easily
 // (not sure if javascript has an easier way to do this, but it works as-is, so...)
@@ -198,6 +42,169 @@ notepad.notes = {};
 (function()
 {
 	"use strict";
+
+	notepad.namedTuple = function(fields, initializer)
+	{
+		if (!Array.isArray(fields))
+			throw new TypeError("Expected array.");
+
+		return function(params)
+		{
+			var object = {};
+
+			if (typeof arguments[0] === "object" && arguments.length < 2)
+			{
+				Object.keys(params).forEach(function(field)
+				{
+					if (fields.indexOf(field) < 0) return;
+					object[field] = params[field];
+				});
+			}
+			else
+			{
+				for (var i=0; i<arguments.length; i++)
+				{
+					if (fields[i] !== undefined)
+						object[fields[i]] = arguments[i];
+				}
+			}
+
+			if (initializer !== undefined)
+			{
+				var newObject = initializer(object);
+				if (newObject) object = newObject;
+			}
+
+			return Object.freeze(object);
+		};
+	};
+
+	notepad.valueFromLetter = function(letter)
+	{
+		var notesInOctave = notepad.notesInOctave;
+
+		var validLetters = "CDEFGAB";
+		var symbol = (letter.length > 1) ? letter[1] : undefined;
+		letter = letter[0].toUpperCase();
+		if (symbol)
+			letter += symbol;
+		var letterIndex = validLetters.indexOf(letter[0]);
+		if (letterIndex < 0)
+			throw new TypeError("Must be a letter between A and G");
+		var halfSteps = [2, 2, 1, 2, 2, 2];
+		var value = 0;
+		for (var i=0; i<letterIndex; i++)
+			value += halfSteps[i];
+		if (symbol === "#")
+			value++;
+		if (symbol === "b")
+			value--;
+		return value % notesInOctave;
+	};
+
+	notepad.makeNote = notepad.namedTuple(["value", "octave"], function(obj)
+	{
+		var valueFromLetter = notepad.valueFromLetter;
+		var notesInOctave = notepad.notesInOctave;
+
+		if (typeof obj.value !== "number")
+		{
+			if (typeof obj.value === "string")
+				obj.value = valueFromLetter(obj.value);
+			else throw new TypeError("\"value\" is a number or a letter");
+		}
+		if (typeof obj.octave !== "number")
+			throw new TypeError("\"octave\" is a number");
+		if (obj.value < 0 || obj.value > 11)
+			throw new RangeError("\"value\" is a number between 0 and 11");
+		if (obj.octave < 0)
+			throw new RangeError("\"octave\" is non-negative");
+
+		obj.toString = function()
+		{
+			var string = "";
+			for (var field in obj)
+			{
+				if (!obj.hasOwnProperty(field)) continue;
+				if (typeof obj[field] === "function") continue;
+				string += obj[field] + " ";
+			}
+			return string;
+		};
+
+		// If new, add to pool of note objects;
+		// otherwise, grab the existing one
+		var string = obj.toString();
+		if (notepad.notes[string] !== undefined)
+			return notepad.notes[string];
+		else notepad.notes[string] = obj;
+
+		obj.fullValue = function()
+		{
+			return this.value + this.octave * notesInOctave;
+		};
+
+		obj.transpose = function(interval)
+		{
+			var makeNote = notepad.makeNote;
+
+			var fullValue = this.fullValue();
+			fullValue += interval;
+			if (fullValue <= 0)
+				return makeNote(0, 0);
+			var newValue = fullValue % notesInOctave;
+			var newOctave = Math.floor(fullValue / notesInOctave);
+			return makeNote(newValue, newOctave);
+		};
+	});
+
+	notepad.notesFromIntervals = function(intervals, baseNote)
+	{
+		var makeNote = notepad.makeNote;
+
+		var notes = [];
+		notes.push(makeNote(baseNote.value, baseNote.octave));
+		intervals.forEach(function(interval)
+		{
+			notes.push(baseNote.transpose(interval));
+		});
+		return notes;
+	};
+
+	notepad.chordIsSubset = function(chord, chordBaseNote, notes)
+	{
+		var notesFromIntervals = notepad.notesFromIntervals;
+
+		var chordNotes = notesFromIntervals(chord, chordBaseNote);
+		// todo: find out why creating this within the loop is wrong
+		var existsTest = function(note)
+		{
+			return note.value === this.value;
+		};
+		for (var note in chordNotes)
+		{
+			if (!chordNotes.hasOwnProperty(note)) continue;
+			if (!notes.some(existsTest, chordNotes[note]))
+				return false;
+		}
+		return true;
+	};
+
+	notepad.compatibleChords = function(scale, scaleBaseNote, chordBaseNote)
+	{
+		var chordIsSubset = notepad.chordIsSubset;
+		var chords = notepad.chords;
+
+		var scaleNotes = notepad.notesFromIntervals(scale, scaleBaseNote);
+		var compatibleList = [];
+		for (var chord in chords)
+		{
+			if (!chords.hasOwnProperty(chord)) continue;
+			if (chordIsSubset(chords[chord], chordBaseNote, scaleNotes))
+				compatibleList.push(chord);
+		}
+		return compatibleList;
+	};
 
 	var makeNote = notepad.makeNote;
 	var scales = notepad.scales;
